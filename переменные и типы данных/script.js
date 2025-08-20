0b559c26de1a94a139aceef354f1489b106c148d
mmnta1221 <kmmnta5@gmail.com>
2025-08-20 11:49:28 +0500

    Add files via upload

---
+++ переменные и типы данных/script.js
@@ -0,0 +1,48 @@
+let name = "Jake"
+console.log(name)
+
+name = "Sofia"
+console.log(name) //может быть изменена
+
+const age = 1
+console.log(age)
+
+// age = 12 
+// console.log(age)  не может быть изменена
+
+//zadanie 2
+//var можкт быть изменена, но ее использование считатся устаревшим тк область видимомти ограничевается функцией или скриптом
+
+// zadanie 3
+
+let number = 10
+let str = "word"
+let boolean = true
+let smth = null
+let not_A_number = NaN
+let thing = undefined
+console.log(number)
+console.log(str)
+console.log(boolean)
+console.log(smth)
+console.log(not_A_number)
+console.log(thing)
+
+//zadanie 4 
+let str2 = "100"
+let str_to_num = Number(str2) //строка стала числом и поменяла цвет
+let num_to_str = String(number) //число стало строкой и поменяло цвет
+console.log(str_to_num )
+console.log(num_to_str)
+
+let undef_to_str = String(undefined) // поменяла цвут и превратилась в строку
+let NaN_to_num = Number(not_A_number) //ничего не изменилось
+console.log(undef_to_str)
+console.log(NaN_to_num)
+
+//zadanie 5
+
+user_name = prompt("Напишите ваше имя")
+console.log(user_name)
+user_age = prompt("Напишите ваш возраст")
+console.log(user_age)
