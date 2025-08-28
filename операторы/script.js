2914c7b47e44a92932a79f942614a50acc04d031
mmnta1221 <kmmnta5@gmail.com>
2025-08-28 11:08:22 +0500

    Add files via upload

--- операторы/script.js
+++ операторы/script.js
@@ -8,14 +8,14 @@ console.log(a%b)
 
 // zadanie 2
 let x = 7
-console.log(++x + x++)
-console.log(--x - x--)
+console.log(x++ + ++x)
+console.log(x-- - --x)
 
 //zadanie 3
 console.log('The result is:' + (4+5))
 
 //zadanie 4
-console.log(a>b>0)
+console.log(a>b && a>0 && b>0)
 console.log(c<10 && c==2)
 console.log((a/c)!= 5)
 
@@ -27,5 +27,5 @@ console.log(num === str) // учитывает тип данных
 
 // zadanie 6
 let y= 6
-console.log(x%2==0 || x%3==0 && x%6 !==0)
+console.log((x%2)==0 || (x%3)==0 && (x%6) !==0)
 console.log(y > 10 || y<5 )
