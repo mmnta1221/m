7b41eb755f3e605592c7269a6854d27f7123825a
mmnta1221 <kmmnta5@gmail.com>
2025-09-18 19:01:10 +0500

    Add files via upload

---
+++ объекты/js.js
@@ -0,0 +1,62 @@
+let person = {
+    age:10,
+    name: "John",
+    city: "New-York"
+
+}
+console.log(person)
+
+///zadanie 2
+
+console.log(person.name, person.age)
+person.city = "Almaty";
+console.log(person.city)
+
+//zadanie 3
+person.greet = "Привет, меня зовут" + " "+ person.name + " "+ "мне" + " " + person.age +"" + "лет!"
+console.log(person.greet)
+
+//zadanie 4
+let obj1 = {
+    a: 10, 
+    b: 20 
+}
+let obj2 = {
+    a: 10, 
+    b: 20 
+}
+console.log(obj1 == obj2)
+console.log(obj1 === obj2) //оба выражениия выводит false т.к == и === проверяют ссылки на объекты, а не их содержимое
+
+//zadanie 5
+let book = {
+    title: "Война и мир",
+    author: "Толстой",
+    details: {
+        year: 1867,
+        pages: 1400 ,
+    }
+}
+let copyBook = Object.assign({}, book)
+book.details.year = 1866
+console.log(book.details.year)
+console.log(copyBook.details.year) //содержимое остается таким же так как details тоже является объектом и копируется лишь ссылка на него, а не значения
+
+//zadanie 6
+let calculator = {
+    a: 10,
+    b: 11,
+    sum(){
+        return this.a+ this.b
+    }
+}
+console.log(calculator.sum())
+
+//zadanie 7
+const car = {
+    brand: "djsidji" ,
+    model: "d12"
+
+}
+car.brand = "hshf"
+console.log(car.brand) // объект изменлся, потому что const не предотвращает изменение содержимого объектов
