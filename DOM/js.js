1ccddd12c768c9e7fc77fe8db6159cae4aa643aa
mmnta1221 <kmmnta5@gmail.com>
2025-09-25 18:46:45 +0500

    Add files via upload

---
+++ DOM/js.js
@@ -0,0 +1,26 @@
+let message = document.getElementById("message");
+message.textContent = "Добро пожаловать в JavaScript!"
+
+//zadanie2
+const color = document.getElementsByClassName('box');
+for( let i =0; i< color.length ; i++){
+    color[i].style.background = 'green';
+}
+
+//zadanie 3
+let text = document.querySelector('#message')
+text.textContent = "wow"
+
+let form = document.querySelectorAll('.box')
+form.forEach(a => {
+    a.style.width = "100px";
+    a.style.height = "100px"
+});
+
+//zadanie 4
+const highlightedParagraphs = document.querySelectorAll('.highlight');
+
+highlightedParagraphs.forEach(paragraph => {
+   paragraph.style.color = 'red';
+});
+
